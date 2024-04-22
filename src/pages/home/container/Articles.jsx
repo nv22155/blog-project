d5f9b6917ee75ae83ba7a2b1c7763e27ab3232c5
nv22155 <155284534+nv22155@users.noreply.github.com>
2024-04-22 18:40:51 +0000

    vido 19

--- src/pages/home/container/Articles.jsx
+++ src/pages/home/container/Articles.jsx
@@ -1,17 +1,34 @@
 import React from "react";
-import ArticleCard from "../../../components/ArticleCard";
 import { FaArrowRight } from "react-icons/fa";
+import { useSelector, useDispatch } from "react-redux";
+import ArticleCard from "../../../components/ArticleCard";
+import { changeCount } from "../../../store/actions/countActions";
+
 const Articles = () => {
+  const dispatch = useDispatch();
+  const count = useSelector((state) => state.count);
+
+  const countChangeHandler = (type) => {
+    dispatch(changeCount(type));
+  };
+
   return (
     <section className="flex flex-col container mx-auto px-5 py-10">
-      <div className=" flex flex-wrap md:gap-x-5 gap-y-5 pb-10">
-        <ArticleCard className="w-full md:w-[calc(50%-20px)] lg:w-[calc(33.33%-21px)]" />
-        <ArticleCard className="w-full md:w-[calc(50%-20px)] lg:w-[calc(33.33%-21px)]" />
+      <div className="flex flex-wrap md:gap-x-5 gap-y-5 pb-10">
+        {/* Render the ArticleCard components */}
+        <ArticleCard />
+        <ArticleCard />
+        <ArticleCard />
       </div>
-      <button className="mx-auto flex items-center gap-x-2 font-bold text-primary border-2 border-primary px-6 py-3 rounded-lg">
+      <button className="flex items-center gap-x-1 text-blue-500">
         <span>More articles</span>
         <FaArrowRight className="w-3 h-3" />
       </button>
+      <div className="mt-2 flex items-center gap-x-5">
+        <button onClick={() => countChangeHandler('DECREASE')}>decrease</button>
+        <span>{count}</span>
+        <button onClick={() => countChangeHandler('INCREASE')}>increase</button>
+      </div>
     </section>
   );
 };
